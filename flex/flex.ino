7a6a0144d3b69f72849f7b3cd9b7f453768ead9c
Lex Raijmakers <lex.raijmakers@gmail.com>
2018-04-18 11:18:53 +0200

    Add files via upload

---
+++ flex/flex.ino
@@ -0,0 +1,19 @@
+//FLEX-SENSOR QUICK START GUIDE
+//PROVIDED BY THE CMD SENSORLAB CREW
+//CREATED ON 18-4-2018
+//V1.0
+//DON'T PANIC
+
+int sensorpin = A0; //ANALOG INPUT
+
+void setup(){
+  Serial.begin(9600); //ARDUINO SERIAL SPEED
+  pinMode(sensorpin, INPUT);  //DECLARE PIN AS INPUT
+}
+
+void loop(){
+  int flexsensor = analogRead(sensorpin); //MAKE INTEGER WITH SENSOR VALUE
+  Serial.println(flexsensor); //PRINT VALUE IN TERMINAL
+  delay(5); //SET DELAY FOR DEBUGGING
+}
+
